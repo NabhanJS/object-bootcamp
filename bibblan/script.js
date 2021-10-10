c9ef1305a1103356d13d1a51c2d5cad589fc388a
NabhanJS <nabhan.abdullah@iths.se>
2021-10-10 16:18:52 +0200

    done with the presentation

--- bibblan/script.js
+++ bibblan/script.js
@@ -1,12 +1,12 @@
-let person = {
+/* let person = {
     name: "Alle",
     email: "kalle@yahoo.com",
     job: "teacher"
-}
+} */
 
 /* console.log(person.email); */
 
-const people = [
+/* const people = [
     {
         name: "Alu",
         email: "alu@yahoo.com",
@@ -31,14 +31,35 @@ const people = [
     }
 
 ]
-
+ */
 /* console.log(people[1].address.ort);
 console.log(people[2].favoritColors[1]); */
 
-let newObject = {};
+/* let newObject = {};
 console.log(newObject);
 newObject.name="Kamal"
 console.log(newObject.name);
 
 newObject["age"] = 23;
-console.log(newObject.age);
+console.log(newObject.age); */
+
+/* const result = {}
+const keys = "abcdef";
+
+for(let key of keys){
+    result[key] = 1;
+
+}
+console.log(result);
+ */
+
+let person = {
+    name: "Salam",
+    job: "Minister"
+}
+
+let convertToJSON = JSON.stringify(person);
+console.log(convertToJSON);
+
+let convertToNormal =JSON.parse(convertToJSON);
+console.log(convertToNormal);
